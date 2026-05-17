e5e8776f9a007cddb988784add1911f867e5bc1a
Tan - Personal <tan-personal@Mac-mini-cua-Tan.local>
2026-05-17 11:16:03 +0700

    Init project

---
+++ openapi-ts.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+  input:
+    'http://localhost:8000/docs-json',
+  output: {
+    path: './src/api/main',
+  },
+  plugins: [
+    '@hey-api/client-axios',
+    '@hey-api/schemas',
+    {
+      dates: true,
+      name: '@hey-api/transformers',
+    },
+    {
+      enums: 'javascript',
+      name: '@hey-api/typescript',
+    },
+    {
+      name: '@hey-api/sdk',
+      transformer: true,
+    },
+  ],
+});
